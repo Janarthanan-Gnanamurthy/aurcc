3f940b752e3f1bb3b4485c952ee5585e3834f92c
spyking05 <ritikrd05@gmail.com>
2024-08-11 10:11:12 +0530

    testing in dept

--- DepartmentsView.vue
+++ DepartmentsView.vue
@@ -186,7 +186,7 @@
           <div id="faculty" class=" mx-auto relative ">
             <h2 class="text-5xl bg-[#3c2813] text-center text-white font-bold p-8">Faculty</h2>
             <!-- HOD Desk -->
-           <div class=" bg-gradient-to-r from-yellow-200 " >
+           <div class=" bg-yellow-200 " >
               <h3 class="text-3xl text-center font-semibold p-10">HOD Desk</h3>
               <div class="flex justify-center">
                 <div 
@@ -210,8 +210,8 @@
 
             <!-- Assistant Professors -->
            <div>
-              <h3 class="text-3xl text-center font-semibold p-14 bg-gradient-to-r from-yellow-200">Assistant Professors</h3>
-              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 bg-gradient-to-r from-yellow-200">
+              <h3 class="text-3xl text-center font-semibold p-14 bg-yellow-200">Assistant Professors</h3>
+              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 bg-yellow-200">
                 <div 
                   v-for="staff in department.faculty.assistant_professors" 
                   :key="staff.email" 
